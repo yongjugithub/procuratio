1c0e35a228ff4ea187aab1ba9c5d8f0ff0743ad6
yongju <yongjujiwork07@gmail.com>
2020-03-18 16:10:53 +0900

    add spec employee model spec

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Employee, type: :model do
   let(:employee) { create(:employee, employee_id: 1, name: 'test user', admin: 'false') }
 
-  describe 'バリデーション' do
+  describe 'モデルのバリデーション' do
     it '乗務員コードと名前があれば有効' do
       expect(employee).to be_valid
     end
@@ -11,6 +11,16 @@ RSpec.describe Employee, type: :model do
     it '重複した乗務員コードでは無効' do
       employee2 = employee.dup
       expect(employee2).not_to be_valid
+    end
+
+    it '名前がなければ、無効' do
+      employee.name = ' '
+      expect(employee).not_to be_valid
+    end
+
+    it '管理者権限をもつことができる' do
+      employee.admin = true
+      expect(employee).to be_valid
     end
   end
 end
