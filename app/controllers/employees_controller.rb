69545083de890e68f72cbe0891a4495fb9fffb10
yongju <yongjujiwork07@gmail.com>
2020-03-18 23:47:35 +0900

    write new and index pages

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,6 +8,16 @@ class EmployeesController < ApplicationController
   end
 
   def new
-    @employee = Employee.new
+    @employee = Employee.new(employee_params)
+    if @employee.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:employee_id, :name, :password, :password_confirmation)
   end
 end
