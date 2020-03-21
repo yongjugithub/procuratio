1cadc1c62e4766455d005925eb9f8156bc34713e
yongju <yongjujiwork07@gmail.com>
2020-03-21 14:14:52 +0900

    amend routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :employees
 end
