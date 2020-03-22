3aee95dde34d64ae291dd6bf5025933f18254487
yongju <yongjujiwork07@gmail.com>
2020-03-22 15:52:06 +0900

    implement invalid input

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def new
-    render 'new'
+  end
+
+  def create
+    employee = Employee.find_by(employee_id: params[:session][:employee_id])
+    if employee && employee.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = '従業員コードとパスワードのどちらか、または両方が無効です'
+      render 'new'
+    end
   end
 end
