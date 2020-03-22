81210c418434edf8408ee8d530a2d612871f4634
yongju <yongjujiwork07@gmail.com>
2020-03-22 17:35:38 +0900

    add session helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,12 @@
 module SessionsHelper
+  #渡されたユーザーでログインする
   def log_in(employee)
     session[:employee_id] = employee.id
+  end
+  #ログインしているユーザーがいる場合、@current_employeeにユーザーを代入する
+  def current_employee
+    if session[:employee_id]
+      @current_employee ||= Employee.find_by(id: session[:employee_id])
+    end
   end
 end
