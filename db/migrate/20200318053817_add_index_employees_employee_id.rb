c2b4e0bdb2375db65e8ee46dbc297f5497e5e7f9
yongju <yongjujiwork07@gmail.com>
2020-04-10 12:42:34 +0900

    edit git ignore

--- db/migrate/20200318053817_add_index_employees_employee_id.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexEmployeesEmployeeId < ActiveRecord::Migration[6.0]
-  def change
-    add_index :employees, :employee_id, unique: true
-  end
-end
--- db/migrate/20200318074433_add_password_digest_to_employees.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordDigestToEmployees < ActiveRecord::Migration[6.0]
-  def change
-    add_column :employees, :password_digest, :string
-  end
-end
